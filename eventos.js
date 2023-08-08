911db900f491263270aa038f9b2243bf8ea879c2
RocioPallas <rocio.pallasm@gmail.com>
2023-08-08 13:23:25 -0300

    Se agregaron nuevos archivos y funcionalidades

---
+++ eventos.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const myDiv = document.getElementById("div-bttn");
+    myDiv.addEventListener("click", (e) => {
+        alert('Hola! Soy el div');
+    });
+});
